897de7a577942a1aaf3141ed76040c7b82963eb4
dongh <dongh2002@qq.com>
2021-11-13 22:01:25 +0800

    basic menu

---
+++ Assets/SubButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubButton : UnityEngine.UI.Button
+{
+    int numMark;
+    public delegate void Action(int numMark);
+    public event Action EventClick;
+
+    protected override void DoStateTransition(SelectionState state, bool instant)
+    {
+        base.DoStateTransition(state, instant);
+        if (state == SelectionState.Pressed)
+            EventClick?.Invoke(numMark);
+    }
+
+    public void MarkAndBind(int numMark, Action action)
+    {
+        this.numMark = numMark;
+        EventClick += action;
+    }
+}
---
+++ Assets/TargetSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public abstract class Target
+{
+    public delegate void Action();
+    public event Action EventTargetComplete;
+    TargetData targetData;
+    public TargetData TargetData { set { targetData = value; } get { return targetData; } }
+    UnityEngine.UI.Text text;
+    public UnityEngine.UI.Text Text { get { return text; } set { text = value; } }
+    public bool IsCompleted { get { return targetData.IsCompleted; }set { bool t = targetData.IsCompleted; targetData.IsCompleted = value; if (value && !t) EventTargetComplete?.Invoke(); } }
+    public abstract void Update();
+    public virtual void UpdateText()
+    {
+        text.text = targetData.description + (targetData.IsCompleted ? "（完成）" : "");
+    }
+}
+
+[System.Serializable]
+public class TestTarget : Target
+{
+    public TestTarget()
+    {
+        EventTargetComplete += base.UpdateText;
+    }
+    public override void Update()
+    {
+        IsCompleted = true;
+        //Debug.Log("targetUpdated");
+    }
+}
